6408350af47b9e4da5684e67a91c9daf31871825
Benjamin W. Bohl <b.w.bohl@gmail.com>
2022-02-28 00:36:38 +0100

    Create zulu-jdkfx-11.rb

---
+++ cask/zulu-jdkfx-11.rb
@@ -0,0 +1,27 @@
+cask "zulu-jdkfx-11" do
+  version "11.0.14.1"
+  #version "11.54.25"
+  sha256 "5d8fbd3f6b7684e3aebbca414cae58dfe1eb544dbf3b60266c5b3f6adad0cb37"
+
+  url "https://cdn.azul.com/zulu/bin/zulu11.54.25-ca-fx-jdk#{version}-macosx_aarch64.dmg"
+  name "JDK FX"
+  desc "The Java platform for the modern cloud enterprise starts with Azul Platform Core, the world's #1 supported build of OpenJDK."
+  homepage "https://www.azul.com/"
+
+  livecheck do
+    url "https://cdn.azul.com/zulu/bin/"
+    strategy :header_match
+    regex(/zulu11\.\d{1,2}\.\d{1,2}-ca-fx-jdk11\.\d{1,2}.\d{1,2}\.\d{1,2}-macosx_aarch64.dmg*/i)
+  end
+
+  auto_updates false
+
+  pkg "Double-Click to Install Azul Zulu JDK 11.pkg"
+
+  zap trash: [
+#    "~/Library/Application Support/Zotero",
+#    "~/Library/Caches/Zotero",
+#    "~/Library/Preferences/org.zotero.zotero-beta.plist",
+#    "~/Library/Saved Application State/org.zotero.zotero-beta.savedState",
+  ]
+end
