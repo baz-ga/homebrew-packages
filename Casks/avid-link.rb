1e0a2b08211b2adefa72d07196ff1062d38c93cd
Benjamin W. Bohl <b.w.bohl@gmail.com>
2022-02-28 12:35:29 +0100

    Update avid-link.rb
    * update version handling in url
    * deactivate livecheck
    * add ununistall
    * add todo after zap

--- Casks/avid-link.rb
+++ Casks/avid-link.rb
@@ -4,32 +4,28 @@ cask "avid-link" do
   version "21.6.1,EM6HW0KF" #21.12.1,5ID6IR1F
   sha256 :no_check
 
-  url "https://cdn.avid.com/AvidLink/2021.6.1/EM6HW0KF/Avid_Link_21.6.1_Mac.dmg"
+  url "https://cdn.avid.com/AvidLink/20#{version.split(',').first}/#{version.split(',').last}/Avid_Link_#{version.split(',').first}_Mac.dmg"
   #url "https://cdn.avid.com/AvidLink/20#{version}/5ID6IR1F/Avid_Link_#{version}_Mac.dmg"
   name "Avid Link"
   desc "Avid Link is a free app for anyone looking to find, connect, and collaborate with other creatives, promote your work and get discovered, and purchase and manage products—all in one interface."
   homepage "https://www.avid.com/products/avid-link"
 
-  livecheck do
-    url "https://www.oxygenxml.com/rssBuildID.xml"
-    strategy :page_match do |page|
-      version = page.match(/Oxygen\sXML\sEditor\sversion\s(\d+(?:\.\d+)+)/i)
-      build = page.match(/build\sid:\s(\d+)/i)
-      next if version.blank? || build.blank?
-
-      "#{version[1]},#{build[1]}"
-    end
-  end
+#TODO check for auto_updates
+#  livecheck do
+#    url ""
+#
+#    end
+#  end
 
   auto_updates true
 
   pkg "AvidLink_Installer.pkg"
 
-  uninstall
+  uninstall script: "/Applications/Avid_Uninstaller/Avid Link/Avid Link Uninstaller.app/Contents/MacOS/AvidUninstaller"
+
   zap trash [
     "~/Library/Preferences/com.avid.link.plist",
     "~/Library/Preferences/com.avid.Avid Link.plist",
     "/Library/Application\ Support/CrashReporter/AvidLink_*.plist"
-    #"/Applications/Avid/Avid Link"
-  ]
+  ] #TODO ? "/Applications/Avid/Avid Link"
 end
