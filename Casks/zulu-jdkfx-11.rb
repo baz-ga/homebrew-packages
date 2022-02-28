7202337bc2f0c8b387acac8f5c0ed7192acbdb74
Benjamin W. Bohl <b.w.bohl@gmail.com>
2022-02-28 11:34:00 +0100

    Update zulu-jdkfx-11.rb
    * make zuluversion comment
    * add capture group around version  in liveckeck
    * add zapdir

--- Casks/zulu-jdkfx-11.rb
+++ Casks/zulu-jdkfx-11.rb
@@ -1,6 +1,6 @@
 cask "zulu-jdkfx-11" do
   version "11.0.14.1"
-  #version "11.54.25"
+  #zulu version "11.54.25"
   sha256 "5d8fbd3f6b7684e3aebbca414cae58dfe1eb544dbf3b60266c5b3f6adad0cb37"
 
   url "https://cdn.azul.com/zulu/bin/zulu11.54.25-ca-fx-jdk#{version}-macosx_aarch64.dmg"
@@ -11,7 +11,7 @@ cask "zulu-jdkfx-11" do
   livecheck do
     url "https://cdn.azul.com/zulu/bin/"
     strategy :header_match
-    regex(/zulu11\.\d{1,2}\.\d{1,2}-ca-fx-jdk11\.\d{1,2}.\d{1,2}\.\d{1,2}-macosx_aarch64.dmg*/i)
+    regex(/zulu11\.\d{1,2}\.\d{1,2}-ca-fx-jdk(11\.\d{1,2}.\d{1,2}\.\d{1,2})-macosx_aarch64.dmg*/i)
   end
 
   auto_updates false
