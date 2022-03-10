f227bf254d6b58d4837306d7108aa7451eb9101a
Benjamin W. Bohl <b.w.bohl@gmail.com>
2022-03-10 01:04:30 +0100

    Update zulujdk-17.rb
    * fix architecture sha256 switch

--- Casks/zulujdk-17.rb
+++ Casks/zulujdk-17.rb
@@ -4,7 +4,7 @@ cask "zulujdk-17" do
   version "17.32.13,17.0.2"
   #zulu-version,java-version
   
-  if arch == ""
+  if arch == "x64"
     sha256 "73d9fd535a690c1fdcc9099edbb7f9e9b8897e30573c0be95360e6896804c203"
   else
     sha256 "74d8e059865fdb8cfbc202bbd93a05c5a67a2d12947b09e30d637dd9b7e4011f"
