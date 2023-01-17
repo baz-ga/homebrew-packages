7ec2a8654855cbc3b9702864519ec0e09722f5c9
Benjamin W. Bohl <b.w.bohl@gmail.com>
2023-01-17 10:34:29 +0100

    Update bazga-oxygen-xml-editor.rb
    remove arch from oxygen download URL

--- Casks/bazga-oxygen-xml-editor.rb
+++ Casks/bazga-oxygen-xml-editor.rb
@@ -1,11 +1,12 @@
 cask "bazga-oxygen-xml-editor" do
-  arch = Hardware::CPU.intel? ? "" : "-aarch64"
+  #arch currently not needed, e.g. in oxygen-openjdk#{arch}.dmg
+  #arch = Hardware::CPU.intel? ? "" : "-aarch64"
 
   #TODO switch between architecture build and sha256
   version "24.1,2022110312" #Build id 2021121415
   sha256 "bd28a3b961e0bb831dd3a3b9cf4999edc4c7738c9284dd8655b3d7c183702f23" #:no_check # md5 "e4dee154352970f3c265e6b79a0bed63"
 
-  url "https://archives.oxygenxml.com/Oxygen/Editor/InstData#{version.split(',').first}/MacOSX/VM/oxygen-openjdk#{arch}.dmg"
+  url "https://archives.oxygenxml.com/Oxygen/Editor/InstData#{version.split(',').first}/MacOSX/VM/oxygen-openjdk.dmg"
   name "oXygen XML Editor"
   desc "Tools for XML editing, including Oxygen XML Developer and Author"
   homepage "https://www.oxygenxml.com/xml_editor.html"
