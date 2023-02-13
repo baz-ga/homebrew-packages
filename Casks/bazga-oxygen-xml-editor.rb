4fedd0aa37ee495b946749745cdcaa972530ef48
Benjamin W. Bohl <b.w.bohl@gmail.com>
2023-02-13 09:46:08 +0100

    Update bazga-oxygen-xml-editor.rb
    fix version for aarch64

--- Casks/bazga-oxygen-xml-editor.rb
+++ Casks/bazga-oxygen-xml-editor.rb
@@ -1,12 +1,13 @@
 cask "bazga-oxygen-xml-editor" do
-  # arch currently not needed, e.g. in oxygen-openjdk#{arch}.dmg
-  # arch = Hardware::CPU.intel? ? "" : "-aarch64"
+  # arch e.g. in oxygen-openjdk#{arch}.dmg
+  arch = Hardware::CPU.intel? ? "" : "-aarch64"
 
   # TODO switch between architecture build and sha256
   version "24.1,2022110312"
   sha256 "bd28a3b961e0bb831dd3a3b9cf4999edc4c7738c9284dd8655b3d7c183702f23"
 
-  url "https://archives.oxygenxml.com/Oxygen/Editor/InstData#{version.split(',').first}/MacOSX/VM/oxygen-openjdk.dmg"
+  #eg  https://archives.oxygenxml.com/Oxygen/Editor/InstData24.1/MacOSX/VM/oxygen-openjdk-aarch64.dmg?_ga=2.253848520.1185206329.1676277524-27673526.1673876391
+  url "https://archives.oxygenxml.com/Oxygen/Editor/InstData#{version.split(',').first}/MacOSX/VM/oxygen-openjdk#{aarch}.dmg"
   name "oXygen XML Editor"
   desc "Tools for XML editing, including Oxygen XML Developer and Author"
   homepage "https://www.oxygenxml.com/xml_editor.html"
