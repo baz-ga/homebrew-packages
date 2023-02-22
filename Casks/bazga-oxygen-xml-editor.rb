62dbd1569115b57e44c72ca3627155f8e4844c63
bwbohl <b.w.bohl@gmail.com>
2023-02-22 12:07:49 +0100

    Update bazga-oxygen-xml-editor.rb
    fix sha

--- Casks/bazga-oxygen-xml-editor.rb
+++ Casks/bazga-oxygen-xml-editor.rb
@@ -4,7 +4,7 @@ cask "bazga-oxygen-xml-editor" do
 
   # TODO switch between architecture build and sha256
   version "24.1,2022110312"
-  sha256 "bd28a3b961e0bb831dd3a3b9cf4999edc4c7738c9284dd8655b3d7c183702f23"
+  sha256 "5f353c0f841ded34ddb42a287d531ec2d5ec23755921b94c31202f642ec651ab"
 
   #eg  https://archives.oxygenxml.com/Oxygen/Editor/InstData24.1/MacOSX/VM/oxygen-openjdk-aarch64.dmg?_ga=2.253848520.1185206329.1676277524-27673526.1673876391
   url "https://archives.oxygenxml.com/Oxygen/Editor/InstData#{version.split(',').first}/MacOSX/VM/oxygen-openjdk#{arch}.dmg"
