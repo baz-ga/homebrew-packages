a9ebad89224f478230a8aa775fca55c0652317a6
bwbohl <b.w.bohl@gmail.com>
2023-02-22 11:48:18 +0100

    Create font-bravura.rb

---
+++ Casks/font-bravura.rb
@@ -0,0 +1,11 @@
+cask 'font-bravura' do	
+    version '1.392'	
+    sha256 :no_check #'abf55d5e01db584c9a00a7395bfc64b667e1b35240a7d188e8eb393a8fbb51b6'
+
+    url "https://github.com/steinbergmedia/bravura/archive/refs/tags/bravura-#{version}.zip"	
+    name 'Bravura'	
+    homepage 'https://www.smufl.org/fonts/'	
+  
+    font 'redist/otf/Bravura.otf'	
+    font 'redist/otf/BravuraText.otf'	
+  end
