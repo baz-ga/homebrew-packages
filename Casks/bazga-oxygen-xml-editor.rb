341cbd5ef73a56f2afe299e527e4eccc7099d66f
Benjamin W. Bohl <b.w.bohl@gmail.com>
2023-04-14 10:03:47 +0200

    Update bazga-oxygen-xml-editor.rb
    implement architecture switch for sha
    
    Signed-off-by: Benjamin W. Bohl <b.w.bohl@gmail.com>

--- Casks/bazga-oxygen-xml-editor.rb
+++ Casks/bazga-oxygen-xml-editor.rb
@@ -1,10 +1,12 @@
 cask "bazga-oxygen-xml-editor" do
   # arch e.g. in oxygen-openjdk#{arch}.dmg
   arch = Hardware::CPU.intel? ? "" : "-aarch64"
+  sha = Hardware::CPU.intel? ? "bd28a3b961e0bb831dd3a3b9cf4999edc4c7738c9284dd8655b3d7c183702f23" : "5f353c0f841ded34ddb42a287d531ec2d5ec23755921b94c31202f642ec651ab"
 
   # TODO switch between architecture build and sha256
   version "24.1,2022110312"
-  sha256 "5f353c0f841ded34ddb42a287d531ec2d5ec23755921b94c31202f642ec651ab"
+  sha256 #{sha}
+  
 
   url "https://archives.oxygenxml.com/Oxygen/Editor/InstData#{version.split(',').first}/MacOSX/VM/oxygen-openjdk#{arch}.dmg"
   name "oXygen XML Editor"
