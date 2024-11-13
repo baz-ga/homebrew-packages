5569c06a632a793d39478bb15d4b29eec4aea1d5
Benjamin W. Bohl <b.w.bohl@gmail.com>
2024-11-13 15:51:03 +0100

    Update avid-link.rb
    Signed-off-by: Benjamin W. Bohl <b.w.bohl@gmail.com>

--- Casks/avid-link.rb
+++ Casks/avid-link.rb
@@ -1,10 +1,11 @@
 cask "avid-link" do
   arch = Hardware::CPU.intel? ? "" : "-aarch64"
   #https://cdn.avid.com/AvidLink/2024.10/10X2QR6Z/Avid_Link_24.10.0_Mac.pkg
+  #https://cdn.avid.com/AvidLink/2024.10/10X2QR6Z/Avid_Link_24.10.0_Mac.dmg
   version "24.10.0,10X2QR6Z" #21.12.1,5ID6IR1F
   sha256 :no_check
 
-  url "https://cdn.avid.com/AvidLink/20#{version.split(',').first[0...-2]}/#{version.split(',').last}/Avid_Link_#{version.split(',').first}_Mac.dmg"
+  url "https://cdn.avid.com/AvidLink/20#{version.split(',').first[0...-2]}/#{version.split(',').last}/Avid_Link_#{version.split(',').first}_Mac.pkg"
   #url "https://cdn.avid.com/AvidLink/20#{version}/5ID6IR1F/Avid_Link_#{version}_Mac.dmg"
   name "Avid Link"
   desc "Avid Link is a free app for anyone looking to find, connect, and collaborate with other creatives, promote your work and get discovered, and purchase and manage products—all in one interface."
